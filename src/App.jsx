04af8da1734ebecaa24ed3d6861b030c5f715f6a
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-01 00:18:13 +0300

    The Flat Lister BMS project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold">The Flat Lister</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <>
+      <h2 className="text-5xl font-bold">The Flat Lister</h2>
+    </>
+  );
+};
+export default Home;
