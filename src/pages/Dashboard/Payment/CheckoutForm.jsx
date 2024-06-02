b8d6e31b79825febb6a2f38249efc90ab0a7f468
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-03 00:59:23 +0300

    Payment implement using stripe

---
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -0,0 +1,114 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
+
+const CheckoutForm = ({ rent, month }) => {
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", { rent }).then((res) => {
+      console.log(res.data.clientSecret);
+      setClientSecret(res.data.clientSecret);
+    });
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+      setError(error.message);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+      setError("");
+    }
+
+    // Confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: user?.displayName || "anonymous",
+            email: user?.email || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log("Confirm Error", confirmError);
+    } else {
+      console.log("Payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Your rent of ${month} is ${paymentIntent.status}`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setTransactionId(paymentIntent.id);
+      }
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        {/* Show error message to your customers */}
+        {error && <div>{error}</div>}
+        <button
+          type="submit"
+          disabled={!stripe || !elements || !clientSecret}
+          className="btn bg-[#D1A054B3] text-white mt-4"
+        >
+          Pay
+        </button>
+      </form>
+      <div>
+        {transactionId && (
+          <p className="text-green-600">
+            Your Transaction ID: {transactionId}{" "}
+          </p>
+        )}
+      </div>
+    </>
+  );
+};
+export default CheckoutForm;
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,17 +1,104 @@
 import { useLocation } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+// TODO: validCoupons will be fetch from database
+const validCoupons = {
+  ONETOWER10: 10,
+  ONETOWER20: 20,
+  ONETOWER30: 30,
+};
+
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_PK);
 
 const Payment = () => {
   const location = useLocation();
-  const { rent, month } = location.state || {};
-  console.log(rent, month);
+  const { rent: currentRent, month } = location.state || {};
+
+  const [rent, setRent] = useState(currentRent);
+  const [coupon, setCoupon] = useState("");
+
+  const applyCoupon = () => {
+    const discount = validCoupons[coupon.toUpperCase()];
+    if (discount) {
+      const discountedRent = currentRent - (currentRent * discount) / 100;
+      setRent(discountedRent);
+      Swal.fire({
+        title: "Coupon Applied!",
+        text: `You received a ${discount}% discount. New rent is $${discountedRent}.`,
+        icon: "success",
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        title: "Invalid Coupon!",
+        text: "Please enter a valid coupon code.",
+        icon: "error",
+        showConfirmButton: true,
+      });
+    }
+  };
+
   return (
     <>
       <SectionTitle title="Payment" subTitle="Pay your rent now" />
-      <div className="flex items-center gap-10 mb-10">
-        <h2 className="text-3xl font-bold">Payment method implement</h2>
+      <div className="bg-[#e87726] rounded-xl p-10 flex justify-center gap-10">
+        <div className="flex-1">
+          <h3 className="text-3xl mb-5">Payment Calculation</h3>
+          {month ? (
+            <div className="space-y-5">
+              <p>Room Rent: ${currentRent}</p>
+              <p>Pay Month: {month}</p>
+
+              <div className="form-control mt-4">
+                <label className="label">
+                  <span className="label-text">Coupon Code</span>
+                </label>
+                <div className="flex">
+                  <input
+                    type="text"
+                    className="input input-bordered mr-2"
+                    value={coupon}
+                    placeholder="Enter a valid coupon..."
+                    onChange={(e) => setCoupon(e.target.value)}
+                  />
+                  <button
+                    type="button"
+                    className="btn bg-[#D1A054B3] text-white"
+                    onClick={applyCoupon}
+                  >
+                    Apply
+                  </button>
+                </div>
+              </div>
+              <p>Payable Rent: ${rent}</p>
+            </div>
+          ) : (
+            <p>No payment details available.</p>
+          )}
+        </div>
+        <div className="flex-1">
+          <h3 className="text-3xl mb-5">Payment Method</h3>
+          <div>
+            <Elements stripe={stripePromise}>
+              <CheckoutForm rent={rent} month={month} />
+            </Elements>
+          </div>
+          {/* Add your payment processing logic here */}
+          {/* <button className="btn bg-[#D1A054B3] text-white mt-4">
+            Pay ${rent}
+          </button> */}
+        </div>
       </div>
     </>
   );
 };
+
 export default Payment;
