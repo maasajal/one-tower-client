7e9c77575142545c031e32dd90b9c03ed75be9e4
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-03 19:53:18 +0300

    Coupon reimplement on payment page

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,30 +2,27 @@ import { useLocation } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle";
 import { useState } from "react";
 import Swal from "sweetalert2";
-
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
-
-// TODO: validCoupons will be fetch from database
-const validCoupons = {
-  ONETOWER10: 10,
-  ONETOWER20: 20,
-  ONETOWER30: 30,
-};
+import useCoupons from "../../../hooks/useCoupons";
 
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_PK);
 
 const Payment = () => {
+  const [coupons] = useCoupons();
   const location = useLocation();
   const { rent: currentRent, month } = location.state || {};
-
   const [rent, setRent] = useState(currentRent);
   const [coupon, setCoupon] = useState("");
-
   const applyCoupon = () => {
-    const discount = validCoupons[coupon.toUpperCase()];
-    if (discount) {
+    // Check the coupon isValid or not | compare to database coupons
+    const validCoupon = coupons.find(
+      (c) => c.coupon_code.toUpperCase() === coupon.toUpperCase()
+    );
+    if (validCoupon) {
+      // Calculate discount & set to rent payment
+      const discount = validCoupon.discount_percentage;
       const discountedRent = currentRent - (currentRent * discount) / 100;
       setRent(discountedRent);
       Swal.fire({
