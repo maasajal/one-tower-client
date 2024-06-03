d372a65c4b7ce7581893035d59fc0778fddd14b6
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-06-03 03:52:29 +0300

    Admin dashboard navigation implement

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,23 +1,53 @@
 import { NavLink, Outlet } from "react-router-dom";
 import logo from "../assets/logo.png";
 const Dashboard = () => {
+  const isAdmin = true;
+
   return (
     <div className="flex">
       <div className="w-64 min-h-screen px-8 pt-12 bg-[#3d5cab] text-white space-y-8 -ml-60 md:ml-0 hover:ml-0 md:hover:ml-0">
         <img src={logo} alt="company logo" />
         <ul className="menu uppercase space-y-4">
-          <li>
-            <NavLink to="/dashboard/myProfile">My Profile</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/makePayment">Make Payment</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/paymentHistory">Payment History</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/announcements">Announcements</NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminProfile">Admin Profile</NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageMembers">Manage Members</NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/makeAnnouncement">
+                  Make Announcement
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/agreementRequests">
+                  Agreement Requests
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageCoupons">Manage Coupons</NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/myProfile">My Profile</NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/makePayment">Make Payment</NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/paymentHistory">
+                  Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/announcements">Announcements</NavLink>
+              </li>
+            </>
+          )}
         </ul>
         <hr />
         <ul className="menu uppercase space-y-4">
